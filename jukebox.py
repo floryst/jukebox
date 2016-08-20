edb7cbfc876a9e63a965bd8b5bfb8a37c41a9a68
Forrest <f@forrestli.com>
2016-08-19 21:27:31 -0400

    initial event pub/sub names

--- jukebox.py
+++ jukebox.py
@@ -3,16 +3,20 @@ log = Logger()
 
 def add(session, url):
     log.info('[jukebox.add]: {url}', url=url)
+    session.publish('com.forrestli.jukebox.event.playlist.add' url)
     return True
 
 def remove(session, song_id):
     log.info('[jukebox.remove]: {song_id}', song_id=song_id)
+    session.publish('com.forrestli.jukebox.event.playlist.remove', song_id)
     return True
 
 def play(session, song_id):
     log.info('[jukebox.play]: {song_id}', song_id=song_id)
+    session.publish('com.forrestli.jukebox.event.player.play', song_id)
     return True
 
 def toggle_pause(session):
     log.info('[jukebox.toggle_pause]')
+    session.publish('com.forrestli.jukebox.event.player.toggle_pause')
     return True
