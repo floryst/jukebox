fd6b5840ff3ceec3ecccfa39c1b348aee5b80744
Forrest <f@forrestli.com>
2016-08-19 21:31:19 -0400

    moved everything into the app component

--- jukebox.py
+++ jukebox.py
@@ -1,22 +1,40 @@
+from twisted.internet.defer import inlineCallbacks
 from twisted.logger import Logger
-log = Logger()
 
-def add(session, url):
-    log.info('[jukebox.add]: {url}', url=url)
-    session.publish('com.forrestli.jukebox.event.playlist.add' url)
-    return True
+from autobahn.twisted.util import sleep
+from autobahn.twisted.wamp import ApplicationSession
+from autobahn.wamp.exception import ApplicationError
 
-def remove(session, song_id):
-    log.info('[jukebox.remove]: {song_id}', song_id=song_id)
-    session.publish('com.forrestli.jukebox.event.playlist.remove', song_id)
-    return True
+class Jukebox(ApplicationSession):
 
-def play(session, song_id):
-    log.info('[jukebox.play]: {song_id}', song_id=song_id)
-    session.publish('com.forrestli.jukebox.event.player.play', song_id)
-    return True
+    log = Logger()
 
-def toggle_pause(session):
-    log.info('[jukebox.toggle_pause]')
-    session.publish('com.forrestli.jukebox.event.player.toggle_pause')
-    return True
+    @inlineCallbacks
+    def onJoin(self, details):
+
+        # jukebox actions
+        yield self.register(self.add, 'com.forrestli.jukebox.add')
+        yield self.register(self.remove, 'com.forrestli.jukebox.remove')
+        yield self.register(self.play, 'com.forrestli.jukebox.play')
+        yield self.register(self.toggle_pause,
+                'com.forrestli.jukebox.toggle_pause')
+
+    def add(self, url):
+        log.info('[jukebox.add]: {url}', url=url)
+        self.publish('com.forrestli.jukebox.event.playlist.add' url)
+        return True
+
+    def remove(self, song_id):
+        log.info('[jukebox.remove]: {song_id}', song_id=song_id)
+        self.publish('com.forrestli.jukebox.event.playlist.remove', song_id)
+        return True
+
+    def play(self, song_id):
+        log.info('[jukebox.play]: {song_id}', song_id=song_id)
+        self.publish('com.forrestli.jukebox.event.player.play', song_id)
+        return True
+
+    def toggle_pause(self):
+        log.info('[jukebox.toggle_pause]')
+        self.publish('com.forrestli.jukebox.event.player.toggle_pause')
+        return True
