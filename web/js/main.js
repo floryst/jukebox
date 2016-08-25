c8ffea366cab66eace0746abd8c66f17d59ba0d2
Forrest <f@forrestli.com>
2016-08-24 21:31:18 -0400

    shims for rewind and forward

--- web/js/main.js
+++ web/js/main.js
@@ -152,6 +152,14 @@
             );
         };
 
+        self.fastRewind = function() {
+            Materialize.toast('Not implemented', 4000);
+        };
+
+        self.fastForward = function() {
+            Materialize.toast('Not implemented', 4000);
+        };
+
         var connection = new autobahn.Connection({
             url: 'ws://127.0.0.1:8080/ws',
             realm: 'realm1'
